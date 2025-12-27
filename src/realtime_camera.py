4ef7a996565359d3fc5d540329ca5ddedaccf8a8
Duaa AbdelAti AbdelAzeem <164356690+DuaA-A@users.noreply.github.com>
2025-12-27 12:38:17 +0200

    update realtime camera --> file import

--- src/realtime_camera.py
+++ src/realtime_camera.py
@@ -1,4 +1,4 @@
-from src.deployment.realtime_detection import run_realtime_detection
+from deployment.realtime_detection import run_realtime_detection
 
 if __name__ == "__main__":
     run_realtime_detection(camera_index=0)
